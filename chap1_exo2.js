096bd98a3a4bf89e9eaee4389ee9084399ada71f
Joz-bruer Quince <jquince@e1z3r1p16.42.us.org>
2019-10-22 16:02:44 -0700

    2e exo

---
+++ chap1_exo2.js
@@ -0,0 +1,15 @@
+/*
+    Ecrivez un programme qui calcule et affiche 
+    le résultat de l'addition, de la soustraction, 
+    de la multiplication et de la division de 6 par 3.
+*/
+
+// Ajoutez vos instructions ici
+// L'addition
+console.log("6 + 3 : Le resultat est : "+ (6 + 3));
+// Soustraction
+console.log("6 - 3 : Le resultat est : "+ (6 - 3));
+// Multiplication
+console.log("6 * 3 : Le resultat est : "+ (6 * 3));
+// Division
+console.log("6 / 3 : Le resultat est : "+ (6 / 3));
